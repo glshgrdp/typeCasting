f46bc8d2e6d52469cc627f8bbbbfe3ca5d0a682e
glshgrdp <gulsahballi@gmail.com>
2025-03-15 01:15:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+//Tür Dönüşümleri
+
+
+        double a = 5.99;
+        int b = (int) a;
+        System.out.println(b);
+    }
+    }
+
